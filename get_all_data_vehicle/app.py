1d362616b85dbef148b4862aeca9045d99517bd8
VictorNoe <20213tn059@utez.edu.mx>
2024-08-05 03:44:27 -0600

    [FIX] get all vehicles

--- get_all_data_vehicle/app.py
+++ get_all_data_vehicle/app.py
@@ -9,19 +9,36 @@ def lambda_handler(event, context):
         database='victor'
     )
 
+    vehicles = []
+
     try:
         with connection.cursor() as cursor:
             sql = "SELECT * FROM vehiculos"
             cursor.execute(sql)
             result = cursor.fetchall()
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Headers': '*',
-                    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-                },
-                'body': json.dumps(result)
-            }
+
+            for row in result:
+                car = {
+                    'id_rate': row[0],
+                    'value': row[1],
+                    'comment': row[2],
+                    'id_auto': row[3],
+                    'id_user': row[4]
+                }
+                vehicles.append(car)
+
     finally:
         connection.close()
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+        },
+        "body": json.dumps({
+            "message": "get vehicles",
+            "data": vehicles
+        }),
+    }
